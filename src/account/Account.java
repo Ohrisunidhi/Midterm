95a5bb647af7add708cf90b9e086d82629c3dda9
Ohrisunidhi <ohrisunidhi2103@gmail.com>
2019-06-19 10:58:59 -0400

    sunidhi

--- src/account/Account.java
+++ src/account/Account.java
@@ -4,65 +4,90 @@
  * and open the template in the editor.
  */
 package account;
-
 /**
  *
  * @author Sivagama
  */
-public class Account {
+public class Account
+{
 
-    
-        private double balance;
-        private double interestRate=0.23;
-        private String user;
- 
-     /**constructor that takes the initial balance
-      * @param initialBalance 
-      */
-        public Account(double initialBalance,String givenUser)
-        {
-            balance=initialBalance;
-            user=givenUser;
-            //Note that the initial balance must be greater than 50.       
-        }
 
-     /**The method for depositing amount and 
-      * updating balance*/
-        public void credit(double amount)
-        {
-            balance=getBalance()+amount;
-        }
+   private double balance;
+   private double interestRate = 0.23;
+   private String user;
 
-     /**The method for debit(withdrawing amount) and 
-      * updating balance */
-        public void debit(double amount)
-        {
-            balance=getBalance()-amount;
+   /** constructor that takes the initial balance
+    *
+    * @param initialBalance
+    */
+   public Account (double initialBalance, String givenUser)
+   {
 
-        }
-        
-     /**The getter for the balance
-     * @return the balance
-     */
-        public double getBalance() 
-        {
-        return balance;
-        }
+      if (initialBalance < 50) {
+         System.out.println("Less amount to start an account ");
+      }
+      else {
 
-    /**The getter for InterestRate
-     * @return the interestRate
-     */
-        public double getInterestRate() 
-        {
-            return interestRate;
-        }
+         balance = initialBalance;
+         user = givenUser;
+         System.out.println("Account Created ");
+         //Note that the initial balance must be greater than 50.       
+      }
+   }
 
-    /**user is the read only field
-     * @return the user
-     */
-    public String getUser() {
-        return user;
-    }
-    
-    
+   /** The method for depositing amount and
+    * updating balance */
+   /**
+    * The method for depositing amount and
+    * updating balance
+    *
+    * @param amount
+    */
+   public void credit (double amount)
+   {
+      balance = getBalance() + amount;
+      System.out.println("Your balance is" + balance);
+   }
+
+   /** The method for debit(withdrawing amount) and
+    * updating balance */
+   public void debit (double amount)
+   {
+      balance = getBalance() - amount;
+
+   }
+
+   /** The getter for the balance
+    *
+    * @return the balance
+    */
+   public double getBalance ()
+   {
+      return balance;
+   }
+
+   /** The getter for InterestRate
+    *
+    * @return the interestRate
+    */
+   public double getInterestRate ()
+   {
+      return interestRate;
+   }
+
+   /** user is the read only field
+    *
+    * @return the user
+    */
+   public String getUser ()
+   {
+      return user;
+   }
+
+   public void interest ()
+   {
+      balance = balance + interestRate;
+
+      System.out.println("the new balance is " + balance);
+   }
 }//class end
